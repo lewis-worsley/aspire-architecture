0b53efd5cde0a4962cfc6f579954407629f11a7b
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-04-10 20:18:11 +0000

    First model created (Project) added

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from project.models import Project
+
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("project_name", "description",)
+    prepopulated_fields = {"slug": ("project_name",)}
+
+admin.site.register(Project, ProjectAdmin)
--- project/models.py
+++ project/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Pending Approval"), (2, "Published"))
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(unique=True, null=False)
+    description = models.TextField()
+    client_name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    finished = models.BooleanField(default=False)
+    tagline = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.project_name
