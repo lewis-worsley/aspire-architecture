90369fd13195d4616ab6dbeeb71c64d48c37d7da
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-04-10 20:31:53 +0000

    Initial content added to the DOM

--- project/views.py
+++ project/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from project.models import Project
 
 # Create your views here.
 
+# def home_view(request):
+#     return render(request, 'base.html')
+
 def home_view(request):
-    return render(request, 'base.html')
+
+    context = {}
+
+    projects = Project.objects.all
+    context['projects'] = projects
+
+    return render(request, 'project/projects.html', context)
