c9961a7ab543002c9d60a07274a0d9fbffe9560a
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-04-12 21:27:21 +0000

    Small changes added to admin panel

--- project/admin.py
+++ project/admin.py
@@ -5,7 +5,9 @@ from project.models import Project
 # Register your models here.
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("project_name", "description",)
+    list_display = ("project_name", "status", "created_on", "updated_on",)
+    search_fields = ("project_name", "description")
     prepopulated_fields = {"slug": ("project_name",)}
+    list_filter = ("status", "created_on")
 
 admin.site.register(Project, ProjectAdmin)
