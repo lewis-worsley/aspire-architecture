d775df9f1299fc3c320b0c89f4f532c259ad1602
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-05-11 12:00:08 +0000

    Auth set up with email and password requirements

--- staff/views.py
+++ staff/views.py
@@ -4,3 +4,19 @@ from django.contrib import messages
 
 
 def login_staff_user(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, "Error occured. Login details incorrect")
+            return redirect('login')
+    
+    else:
+            # Return an 'invalid login' error message.
+        return render(request, 'staff/login.html', {})
+
