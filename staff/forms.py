7b87b653f774218aed6daf703ed9bee1a875ab66
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-05-17 10:24:28 +0000

    Email validator added to email field

--- staff/forms.py
+++ staff/forms.py
@@ -1,13 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.validators import RegexValidator
 
 
 class RegisterStaffUserForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=90, help_text="Must register with company email domain '@aspire.com'")
+    email = forms.EmailField(
+        max_length=90,
+        help_text="Must register with company email domain '@aspire.com'",
+        validators=[
+            RegexValidator(
+                regex='^^\w+@aspire\.com$',
+                message=
+                'Invalid email address. Please use the @aspire.com domain',
+                code='invalid email address'
+            ),
+        ])
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields =
+        ('username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2')
--- staff/views.py
+++ staff/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+import re
+
 from .forms import RegisterStaffUserForm
 
 
+# Linked to login.html
 def login_staff_user(request):
 
     user = request.user
@@ -20,22 +23,28 @@ def login_staff_user(request):
 
         if user is not None:
             login(request, user)
-            messages.success(request, f"Login successful! Hello {username}.")
+            messages.success
+            (request,
+                f"Login successful! Hello {username}.")
             return redirect('home')
         else:
-            messages.error(request, "Error occured. Login details incorrect. Invalid username or password.")
+            messages.error
+            (request,
+                "Login details incorrect. Invalid username or password.")
             return redirect('login')
 
     else:
         return render(request, 'staff/login.html', {})
 
 
+# Not linked to any page but logout can be found in the footer
 def logout_staff_user(request):
     logout(request)
     messages.success(request, "You're now logged out!")
     return redirect('home')
 
 
+# Linked to register.html
 def register_staff_user(request):
 
     user = request.user
@@ -47,16 +56,24 @@ def register_staff_user(request):
         form = RegisterStaffUserForm(request.POST)
 
         if form.is_valid():
+            email = form.cleaned_data['email']
+
+            # Check if email matches the desired pattern
+            pattern = r'^\w+@aspire\.com$'
+
+            if not re.match(pattern, email):
+                messages.error
+                (request,
+                    "Invalid email address. Please use the @aspire.com domain")
+                return render(request, 'staff/register.html', {'form': form})
+
             form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            messages.info(request, "Your account is pending approval. Admin has been notified of your request.")
+            messages.info
+            (request,
+                "Account pending approval. Admin has been notified.")
             return redirect('home')
-        else:
-            for error in list(form.errors.values()):
-                messages.error(request, error)
 
     else:
         form = RegisterStaffUserForm()
-        
+
     return render(request, 'staff/register.html', {'form': form})
