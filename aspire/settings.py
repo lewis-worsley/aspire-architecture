74a6a1f2c5663a7834c173e0cb4315c000145a1c
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-05-19 15:59:41 +0000

    X_FRAME_OPTIONS variable added

--- aspire/settings.py
+++ aspire/settings.py
@@ -43,6 +43,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['https://aspire-architecture.herokuapp.com/', 'localhost']
 
 
